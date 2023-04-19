b46511f496ba2e0a4902b0ae24b2b38e39ffe7fd
Thiago Cruz <thiagocruz.eu@gmail.com>
2023-04-19 15:58:21 -0300

    feat: create app header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, InputSearchContainer } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts app logo" width="201" />
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+    </Container>
+  );
+}
