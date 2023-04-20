270b6b9e90cd472a5313cbc12e1438c51696af03
Thiago Cruz <thiagocruz.eu@gmail.com>
2023-04-20 17:22:36 -0300

    style: update global style variables

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,11 @@
 export default {
-  backgroundColor: '#f6f5fe',
+  colors: {
+    background: '#f6f5fe',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+  },
 };
